957d99986d99dd73b551a028bc5ca5219339d670
Tassneem04Hamdy <tassneemhamdy@gmail.com>
2023-02-19 16:12:13 +0200

    eslint rules added

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-  .requiredOption('--contest <contest-id>', 'cf contest id')
-  .requiredOption('--api-key <api-key>', 'cf api key');
+	.requiredOption('--contest <contest-id>', 'cf contest id')
+	.requiredOption('--api-key <api-key>', 'cf api key');
 
 program.parse(process.argv);
 
